aa9eb95deff35ae50b81d68c5d02fd271bff972b
unknown <abdallah.abdellatif00@gmail.com>
2020-07-12 21:08:54 +0200

    newwwww

---
+++ webpack.prod.js
@@ -0,0 +1,32 @@
+const path = require("path")
+const webpack = require("webpack")
+const HtmlWebPackPlugin=require("html-webpack-plugin")
+module.exports = {
+    mode:'production',
+    entry: './src/client/index.js',
+    module: {
+        rules: [
+                {
+            test: '/\.js$/',
+            exclude: /node_modules/,
+            loader: "babel-loader"
+                },
+                {
+                    test: /\.scss$/,
+                    use: [ 'style-loader', 'css-loader', 'sass-loader' ]
+                }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/client/views/index.html",
+            filename: "./index.html"
+        })
+    ],
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist/'),
+        libraryTarget: 'var',
+        library: 'Client'
+    }
+}
